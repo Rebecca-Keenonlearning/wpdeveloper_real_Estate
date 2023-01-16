3b35b261afcf362c1f1ad3273227c643d4c5fe3f
Becky Zhao <dev@thewebdesign.us>
2023-01-16 13:23:48 -0800

    added drilldrop for all skills, but still need to revise and add more

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,15 +1,29 @@
-document.querySelector('#css_skills').onclick = function () {
+document.querySelector('#photoshop_skills').onclick = function () {
     // document.querySelector('#bootstrap_more_skills').show();
 
     // $('#bootstrap_more_skills').fadeIn('slow');
 
-    if ($('#css_more_skills').is(":visible")) {
-        $('#css_more_skills').fadeOut('slow');
+    if ($('#photoshop_more_skills').is(":visible")) {
+        $('#photoshop_more_skills').fadeOut('slow');
     } else {
-        $('#css_more_skills').slideDown('slow');
+        $('#photoshop_more_skills').slideDown('slow');
 
     }
 }
+
+document.querySelector('#html_skills').onclick = function () {
+    // document.querySelector('#bootstrap_more_skills').show();
+
+    // $('#bootstrap_more_skills').fadeIn('slow');
+
+    if ($('#html_more_skills').is(":visible")) {
+        $('#html_more_skills').fadeOut('slow');
+    } else {
+        $('#html_more_skills').slideDown('slow');
+
+    }
+}
+
 
 document.querySelector('#css_skills').onclick = function () {
     // document.querySelector('#bootstrap_more_skills').show();
@@ -47,6 +61,71 @@ document.querySelector('#bootstrap_skills').onclick = function () {
         $('#bootstrap_more_skills').fadeOut('slow');
     } else {
         $('#bootstrap_more_skills').slideDown('slow');
+
+    }
+}
+
+document.querySelector('#javascript_skills').onclick = function () {
+    // document.querySelector('#bootstrap_more_skills').show();
+
+    // $('#bootstrap_more_skills').fadeIn('slow');
+
+    if ($('#javascript_more_skills').is(":visible")) {
+        $('#javascript_more_skills').fadeOut('slow');
+    } else {
+        $('#javascript_more_skills').slideDown('slow');
+
+    }
+}
+
+document.querySelector('#wordpress_skills').onclick = function () {
+    // document.querySelector('#bootstrap_more_skills').show();
+
+    // $('#bootstrap_more_skills').fadeIn('slow');
+
+    if ($('#wordpress_more_skills').is(":visible")) {
+        $('#wordpress_more_skills').fadeOut('slow');
+    } else {
+        $('#wordpress_more_skills').slideDown('slow');
+
+    }
+}
+
+document.querySelector('#jquery_skills').onclick = function () {
+    // document.querySelector('#bootstrap_more_skills').show();
+
+    // $('#bootstrap_more_skills').fadeIn('slow');
+
+    if ($('#jquery_more_skills').is(":visible")) {
+        $('#jquery_more_skills').fadeOut('slow');
+    } else {
+        $('#jquery_more_skills').slideDown('slow');
+
+    }
+}
+
+document.querySelector('#git_github_skills').onclick = function () {
+    // document.querySelector('#bootstrap_more_skills').show();
+
+    // $('#bootstrap_more_skills').fadeIn('slow');
+
+    if ($('#git_github_more_skills').is(":visible")) {
+        $('#git_github_more_skills').fadeOut('slow');
+    } else {
+        $('#git_github_more_skills').slideDown('slow');
+
+    }
+}
+
+document.querySelector('#php_skills').onclick = function () {
+    // document.querySelector('#bootstrap_more_skills').show();
+
+    // $('#bootstrap_more_skills').fadeIn('slow');
+
+    if ($('#php_more_skills').is(":visible")) {
+        $('#php_more_skills').fadeOut('slow');
+    } else {
+        $('#php_more_skills').slideDown('slow');
 
     }
 }
